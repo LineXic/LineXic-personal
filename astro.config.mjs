b294b6d610074446d366a4cf4e47cc089795f2a3
文心 <121679359+LineXic@users.noreply.github.com>
2024-12-14 23:43:39 +0800

    更新 astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import tailwind from '@astrojs/tailwind.config.mjs';
+import tailwind from '/tailwind.config.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'http://i.linexic.top',
     integrations: [mdx(), sitemap(), tailwind()],
 });
