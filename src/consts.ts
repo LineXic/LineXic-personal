c5a4fea0e6c6d1158daaff8bf7f4fe4b524a8e0b
文心 <121679359+LineXic@users.noreply.github.com>
2024-12-14 16:08:39 +0800

    Add files via upload

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'LineXic';
+export const SITE_DESCRIPTION = '我们都会像繁星一样灿烂的活着';
