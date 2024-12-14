c690cb3219838a3502ab4791f6c50e530f7decb2
文心 <121679359+LineXic@users.noreply.github.com>
2024-12-15 07:56:18 +0800

    更新 astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import tailwind from 'https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap-grid.min.css';
-
 // https://astro.build/config
 export default defineConfig({
     site: 'https://i.linexic.top',
